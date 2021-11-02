569bb9df0a5887df41820c4cac7d940f970b4f27
Anton Kazachenka <Kazachenkoanton98@gmail.com>
2021-11-02 21:58:42 +0300

    change 1

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 const calc = ( ident,a,b ) => {
-    if( ident && a && b || (a && b) !== Number(a) && Number(b) ) {
+    if( ident && a && b ) {
         if ( isNaN(a + b) ) {
             return "error"
         }
-        if( ident === 'sum' ) {
-            return a + b
-        } else if( ident === 'remains' ) {
-            return a % b
-        } else if( ident === 'subtract' ) {
-            return a - b
-        } else if( ident === 'exponent' ) {
-            return a ** b
-        } else if( ident === 'multiplication' ) {
-            return a * b
-        } else if( ident === 'division' ) {
-            return a / b
-        } else {
-            return 'unknow operation'
-        } 
+        switch(ident){
+        case 'sum':
+            return a + b;
+        case 'remains':
+            return a % b;
+        case 'subtract':
+            return a - b;
+        case 'exponent':
+            return a ** b;
+        case 'multiplication':
+            return a * b;
+        case 'division':
+            return a / b;
+        default:
+            return 'unknow operation';
+        }
     
     } else {
         return 'error'
